df07d1a66ec0630e9b6054f8a26f5996569680ad
Волков Борис <volkovboris1501@gmail.com>
2026-09-25 23:52:20 +0300

    Lab05: advancedswitch

--- Program.cs
+++ Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿// int dayNumber = 7;
+// switch (dayNumber)
+// {
+//     case 5 or 6 or 7: System.Console.WriteLine("Выходной"); break;
+//     default: System.Console.WriteLine("Будний"); break;
+// }
+
+// int score =  101;
+// switch (score)
+// {
+//     case >= 0 and <= 39: System.Console.WriteLine("Неудовлетворительно"); break;
+//     case >= 40 and < 59: System.Console.WriteLine("Удовлетворительно"); break;
+//     case >= 60 and < 79: System.Console.WriteLine("Хорошо"); break;
+//     case >= 80 and <= 100: System.Console.WriteLine("Отлично"); break;
+//     default: System.Console.WriteLine("Некорректный балл"); break;
+// }
+
+// int score = 30;
+// string result = score switch
+// {
+//     < 0 => "Мороз",
+//     >= 0 and <= 14 => "Прохладно",
+//     >= 15 and <= 24 => "Комфортно",
+//     >= 25 and <= 34 => "Жарко",
+//     >= 35 => " Очень жарко",
+// };
+// System.Console.WriteLine(result);
